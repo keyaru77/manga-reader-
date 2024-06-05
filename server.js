5b83cd5b5304130bdf5ec2d23c3641d14edd36af
keyaru77 <whyudacok@gmail.com>
2024-06-06 00:51:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const axios = require('axios');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const BASE_URL = 'https://manga-reader-mauve.vercel.app/';
+
+// Import routes
+const mangaRouter = require('./src/manga');
+const chapterRouter = require('./src/chapter');
 
 // Set view engine
 app.set('view engine', 'ejs');
@@ -13,37 +15,12 @@ app.set('views', path.join(__dirname, 'views'));
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use routes
+app.use('/manga', mangaRouter);
+app.use('/chapter', chapterRouter);
+
 app.get('/', (req, res) => {
   res.render('index');
-});
-
-app.get('/manga/:endpoint', async (req, res) => {
-  const { endpoint } = req.params;
-  try {
-    const response = await axios.get(`https://asuanjg.vercel.app/manga/${endpoint}`);
-    let data = response.data;
-
-    // Replace base URL in the data
-    const replaceBaseUrl = (url) => url.replace('https://komikcast.cx', BASE_URL);
-
-    data.firstChapter.link = replaceBaseUrl(data.firstChapter.link);
-    data.lastChapter.link = replaceBaseUrl(data.lastChapter.link);
-
-    data.chapters = data.chapters.map(chapter => ({
-      ...chapter,
-      chapterLink: replaceBaseUrl(chapter.chapterLink),
-      downloadLink: replaceBaseUrl(chapter.downloadLink)
-    }));
-
-    data.relatedManga = data.relatedManga.map(manga => ({
-      ...manga,
-      href: replaceBaseUrl(manga.href)
-    }));
-
-    res.render('manga', { data });
-  } catch (error) {
-    res.status(500).send('Error fetching manga data');
-  }
 });
 
 app.listen(PORT, () => {
