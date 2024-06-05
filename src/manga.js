ae83b32af29bbb98a48560a3ef9b3720698eec6a
keyaru77 <whyudacok@gmail.com>
2024-06-06 00:56:13 +0700

    Update manga.js

--- src/manga.js
+++ src/manga.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
-const BASE_URL = 'https://manga-reader-mauve.vercel.app';
+const BASE_URL = 'https://manga-reader-mauve.vercel.app/chapter';
 
 router.get('/:endpoint', async (req, res) => {
   const { endpoint } = req.params;
